6baef4e6e85ac2443cb41808c2ad74cde4ffce46
Miguel Scalet <miguelscalet2000@gmail.com>
2026-06-03 14:58:47 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBfyYcYP-mcLbWVEk3iX9iAT9xH-sRw5jw",
+  authDomain: "dsjquery-b5fec.firebaseapp.com",
+  databaseURL: "https://dsjquery-b5fec-default-rtdb.firebaseio.com",
+  projectId: "dsjquery-b5fec",
+  storageBucket: "dsjquery-b5fec.firebasestorage.app",
+  messagingSenderId: "1023731158081",
+  appId: "1:1023731158081:web:bccab661af4b3466ce4ac0",
+  measurementId: "G-3G93N6K0EH"
+};
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
